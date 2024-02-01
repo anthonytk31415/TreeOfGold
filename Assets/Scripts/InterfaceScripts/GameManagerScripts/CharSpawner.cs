3f5b2b5f75b49d8bf321529b4486ff8beec77848
anthonytk31415 <anthonytk31415@gmail.com>
2024-02-01 13:14:00 -0800

    updated settings for no bugs

--- Assets/Scripts/InterfaceScripts/GameManagerScripts/CharSpawner.cs
+++ Assets/Scripts/InterfaceScripts/GameManagerScripts/CharSpawner.cs
@@ -6,20 +6,20 @@
 // using UnityEngine;
 
 
-// // public class CharSpawner : MonoBehaviour
-// // {
-// //     public void SpawnChar(String pfFileName, int hp, int attack, Double x, Double y)
-// //     {
-// //         GameObject charPrefab = Resources.Load("Prefabs/Characters/" + pfFileName, typeof(GameObject)) as GameObject;
-// //         GameObject charInstance = Instantiate(charPrefab, new Vector3((float)x, (float)y, 0.0f), Quaternion.identity);
+// public class CharSpawner : MonoBehaviour
+// {
+//     // public void SpawnChar(String pfFileName, int hp, int attack, Double x, Double y)
+//     // {
+//     //     GameObject charPrefab = Resources.Load("Prefabs/Characters/" + pfFileName, typeof(GameObject)) as GameObject;
+//     //     GameObject charInstance = Instantiate(charPrefab, new Vector3((float)x, (float)y, 0.0f), Quaternion.identity);
 
-// //         // Add components (scripts) to the char instance
-// //         charInstance.AddComponent<CharacterStats>();
-// //         CharacterStats charStats = charInstance.GetComponent<CharacterStats>();
+//     //     // Add components (scripts) to the char instance
+//     //     charInstance.AddComponent<CharacterStats>();
+//     //     CharacterStats charStats = charInstance.GetComponent<CharacterStats>();
 
-// //         // assign stats to char
-// //         charStats.hp = hp;
-// //         charStats.attack = attack;
-// //     }
+//     //     // assign stats to char
+//     //     charStats.hp = hp;
+//     //     charStats.attack = attack;
+//     // }
 
-// // }
+// }
