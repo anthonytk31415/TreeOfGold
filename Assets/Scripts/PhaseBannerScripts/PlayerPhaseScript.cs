a0a3ba7bafdf8920ed576efa3e41dfa03590329d
anthonytk31415 <anthonytk31415@gmail.com>
2024-03-23 11:24:04 -0700

    completed fading playerText screen

--- Assets/Scripts/PhaseBannerScripts/PlayerPhaseScript.cs
+++ Assets/Scripts/PhaseBannerScripts/PlayerPhaseScript.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,16 +21,28 @@ public class PlayerPhaseScript : MonoBehaviour
     public static IEnumerator InstantiatePlayerPhaseObject(){
         GameObject playerPhase = Resources.Load("Prefabs/PhaseBanner/PlayerPhaseCanvas", typeof(GameObject)) as GameObject;        
         GameObject playerPhaseInstance = Instantiate(playerPhase);
-        Transform playerPhaseTextTransform = playerPhaseInstance.transform.Find("PlayerPhaseText");
-        Debug.Log(playerPhaseTextTransform);
+        Transform playerPhaseTextTransform = playerPhaseInstance.transform.Find("BackgroundPanel/PlayerPhaseText");
         if (playerPhaseTextTransform != null){
-            Debug.Log(playerPhaseTextTransform.GetComponent<Text>());
+            Debug.Log(playerPhaseTextTransform.GetComponent<TextMeshProUGUI>().color);
+            TextMeshProUGUI textObj = playerPhaseTextTransform.GetComponent<TextMeshProUGUI>(); 
+            yield return FadeToAlpha(0.5f, textObj);
         }
-        yield return new WaitForSeconds(0.5f);
-
         Destroy(playerPhaseInstance);
         yield return new WaitForSeconds(0.5f);
-
     
+    }
+
+    public static IEnumerator FadeToAlpha(float fadeDuration, TextMeshProUGUI textObj){
+        float ticks = 10f; 
+        float tickUnit = fadeDuration/ticks; 
+        float curTick = 0f; 
+        while (curTick < fadeDuration)
+        {
+            float percentageComplete = curTick/fadeDuration; 
+            float pctAlpha = 1-percentageComplete; 
+            textObj.alpha = pctAlpha;
+            curTick += tickUnit;
+            yield return new WaitForSeconds(tickUnit);
+        }
     }
 }
